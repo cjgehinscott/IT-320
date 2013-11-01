4c9eae3effe45823d72e093e312ad4eb581d356d
CJ Gehin-Scott <cjgehinscott@me.com>
2013-11-01 18:11:02 -0400

    Initial Commit

---
+++ C++Example/C++Example/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  C++Example
+//
+//  Created by CJ Gehin-Scott on 11/1/13.
+//  Copyright (c) 2013 IT320. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
